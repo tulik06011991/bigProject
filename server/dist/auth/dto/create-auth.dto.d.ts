c792639dc558ebb6d74b1d3e2b9fa92b4d1c1321
tolqin <tolqinmirsaliyev@gmail.com>
2024-11-15 11:27:51 +0500

    sdsdsds

--- server/dist/auth/dto/create-auth.dto.d.ts
+++ server/dist/auth/dto/create-auth.dto.d.ts
@@ -1,2 +1,6 @@
 export declare class CreateAuthDto {
+    name: string;
+    email: string;
+    password: string;
+    createdAt?: string;
 }
--- server/src/auth/dto/create-auth.dto.ts
+++ server/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,17 @@
-export class CreateAuthDto {}
+import { IsString, MinLength, IsEmail,  IsOptional, IsDateString } from "class-validator";
+
+export class CreateAuthDto {
+@IsString()
+name: string;
+@IsEmail()
+email: string;
+@IsString()
+@MinLength(6)
+password: string;
+
+@IsOptional()
+@IsDateString() // Ixtiyoriy, lekin foydalanuvchi yubormasligi kerak
+createdAt?: string;
+
+
+}
